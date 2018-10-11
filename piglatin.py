3cc012c583fe6708b82aa0ee899740a35eb4904c
Eric Ndung'u <ericobain@gmail.com>
2018-10-11 23:25:40 +0300

    building the stack on top of a list

---
+++ piglatin.py
@@ -0,0 +1,6 @@
+# piglatin.py
+
+class Stack:
+    """A class implementation of the Stack data structure."""
+    def __init__(self):
+        self._items = []
--- test_piglatin.py
+++ test_piglatin.py
@@ -5,7 +5,17 @@ import unittest
 from piglatin import Stack
 
 
-class StackTests(unitetest.TestCase):
+class StackTests(unittest.TestCase):
 
     def setUp(self):
         self.stack = Stack()
+    
+    def test_initialized_stack_has_list(self):
+        """Test that an empty list is an attribute of the new initialized
+        stack.
+        """
+        self.assertEqual(self.stack._items, [])
+
+
+if __name__ == "__main__":
+        unittest.main(warnings="ignore")
